ae09e533cb05c7728c6180bb297f6f40f1d09b60
fredt00 <frederick.thompson@physics.ox.ac.uk>
2024-08-12 14:06:59 +0100

    fix output

--- examples/fred/galaxy_cluster_master.py
+++ examples/fred/galaxy_cluster_master.py
@@ -305,7 +305,7 @@ def main(star_cluster_number_of_workers = 2, galaxy_force_number_of_workers = 0,
         # save output
         if integrator.time.value_in(units.Myr) % output_interval.value_in(units.Myr)==0:
             # cluster.transfer_unbound_particles()
-            print("cluster position in galactic frame",cluster.particles.position.in_(units.kpc))
+            print("cluster position in galactic frame",cluster.particles.center_of_mass().in_(units.kpc))
             if not analytic:
                 io.write_set_to_file( galaxy.particles,'galaxy_'+restart_file+".hdf5",'hdf5', timestamp=integrator.time, append_to_file=True)
             if cluster_model:
