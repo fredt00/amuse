68a69b7092290d3709bcb44645ddd55a8af893e9
fredt00 <frederick.thompson@physics.ox.ac.uk>
2024-08-14 10:14:50 +0100

    new unbound method

--- src/amuse/plot/pickle_cluster.py
+++ src/amuse/plot/pickle_cluster.py
@@ -53,7 +53,7 @@ def amuse_cluster(filename, data):
         # then this particle is not included in energy determination of the next particle
         # ones deemed unbound can probably be removed from the next loop too
         if len(unbound_particles) > 0:
-            bound = cluster.remove_particles(unbound_particles)
+            bound = cluster.copy().remove_particles(unbound_particles)
         else:
             bound = cluster.copy()
         cmx, cmy, cmz = bound.center_of_mass()
