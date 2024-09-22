ee1f3cf9edd081413917913fd6d2a0b32c120396
fredt00 <frederick.thompson@physics.ox.ac.uk>
2024-09-22 20:45:12 +0100

    improved data analysis - now does same bound/unbound calc as petar

--- src/amuse/plot/pickle_cluster.py
+++ src/amuse/plot/pickle_cluster.py
@@ -22,10 +22,10 @@ from amuse.ext.galactic_potentials import MWpotentialBovy2015
 from amuse.ext.derived_grav_systems import tidal_field
 import sys
 from amuse.community.fastkick.interface import FastKick
+from amuse.community.ph4.interface import ph4
 from amuse.datamodel import ParticlesSuperset
 import argparse
 from amuse.datamodel.particle_attributes import HopContainer
-from amuse.community.ph4.interface import ph4
 
 sys.setrecursionlimit(10000)
 
@@ -45,23 +45,27 @@ sys.setrecursionlimit(10000)
 def amuse_cluster(filename, galaxy_filename,data):
     print('about to read '+ filename)
     data_cluster = io.read_set_from_file(filename, close_file=True)
-    data_galaxy = io.read_set_from_file(galaxy_filename, close_file=True)
+    # data_galaxy = io.read_set_from_file(galaxy_filename, close_file=True)
     i=0
     unbound_particles = Particles()
-    for cluster, galaxy in zip(data_cluster.history, data_galaxy.history):
+    for cluster in data_cluster.history:
         t_snap = cluster.get_timestamp().in_(units.Myr)
+        if t_snap==460|units.Myr: continue # skip problem for now
         print(t_snap.in_(units.Myr))
 
-        gal_converter = nbody_system.nbody_to_si(galaxy.total_mass(), galaxy.total_radius())
-        galaxy_force_field = FastKick(gal_converter, number_of_workers=20)
-        galaxy_force_field.particles.add_particles(galaxy)
-        galaxy_force_field.parameters.epsilon_squared=gal_converter.to_nbody((100 | units.pc)**2)
+        
+        galaxy_force_field = MWpotentialBovy2015()
+        
         gal_field = tidal_field(galaxy_force_field)
         # remove previous unbound particles
+        print("pre removal", len(cluster))
         cluster.remove_particles(unbound_particles)
-
+        if len(cluster) <100:
+            break
+        print("post removal", len(cluster))
         # compute who is bound and who isn't - note this is for old version where the code hasn't already done this for us
         converter= nbody_system.nbody_to_si(cluster.total_mass(), cluster.total_radius())
+
         # define a fastkick instance that we will use for all our potential calculations
         computer = ph4(converter, number_of_workers=23)
         computer.particles.add_particles(cluster)
@@ -94,8 +98,8 @@ def amuse_cluster(filename, galaxy_filename,data):
             else:
                 break
 
-        galaxy_force_field.stop()
         print("number of bound particles", len(cluster))
+        print("number of unbound particles", len(unbound_particles))
 
         data['time'].append(t_snap)
         data['mass'].append(cluster.mass.sum())
@@ -106,13 +110,100 @@ def amuse_cluster(filename, galaxy_filename,data):
         rhalf = cluster.LagrangianRadii(mf=[0.5])[0][0]
         data['rhalf'].append(rhalf)
         
+        # this could happen in parallel - would need threadfence at end
         potential_energy = (computer.particles.mass*computer.particles.potential_in_code).sum()
+      
 
         inside = cluster.position.lengths() < rhalf
         E = cluster.kinetic_energy() + potential_energy
         data["E"].append(E)
         data['psi'].append((cluster.mass[inside]**(5/2)).mean()/cluster.mass[inside].mean()**(5/2))
         data['kappa'].append(-E*rhalf/(constants.G*cluster.mass.sum()**2))
+        computer.stop()
+    return data
+
+def amuse_cluster_new(filename, galaxy_filename,data):
+    print('about to read '+ filename)
+    data_cluster = io.read_set_from_file(filename, close_file=True)
+    if galaxy_filename:
+        data_galaxy = io.read_set_from_file(galaxy_filename, close_file=True)
+        zipped = zip(data_cluster.history, data_galaxy.history)
+    else:
+        zipped = zip(data_cluster.history, data_cluster.history)
+    for cluster, galaxy in zipped:
+        t_snap = cluster.get_timestamp().in_(units.Myr)
+        print(t_snap.in_(units.Myr))
+
+        if galaxy_filename:
+            gal_converter = nbody_system.nbody_to_si(galaxy.total_mass(), galaxy.total_radius())
+            galaxy_force_field = FastKick(gal_converter, number_of_workers=20)
+            galaxy_force_field.particles.add_particles(galaxy)
+            galaxy_force_field.parameters.epsilon_squared=gal_converter.to_nbody((100 | units.pc)**2)
+        else:
+            galaxy_force_field = MWpotentialBovy2015()
+        
+        gal_field = tidal_field(galaxy_force_field)
+        
+        # compute who is bound and who isn't - note this is for old version where the code hasn't already done this for us
+        converter= nbody_system.nbody_to_si(cluster.total_mass(), cluster.total_radius())
+
+        # define a fastkick instance that we will use for all our potential calculations
+        computer = ph4(converter, number_of_workers=23)
+        computer.particles.add_particles(cluster)
+            # the particles in the framework that are currently defined as bound
+            # find the centre of mass
+        core = computer.particles.cluster_core(converter, density_weighting_power=2, reuse_hop=False, hop=HopContainer())
+        position=computer.particles.position-core.position
+        r2=position.lengths_squared()
+
+        # find the particles outside the tidal radius - only compute energy of these
+        Mgal = -galaxy_force_field.get_potential_at_point(0 | units.pc,core.position.x, core.position.y, core.position.z) * core.position.length()/constants.G
+        tidal_radius_phi = (computer.particles.total_mass()/(3*Mgal))**(1/3) * core.position.length()
+        tidal_radius = gal_field.tidal_radius(4|units.pc, core.position.x, core.position.y, core.position.z, computer.particles.total_mass())
+        print("from tensor", tidal_radius.in_(units.pc))
+        print("from potential", tidal_radius_phi.in_(units.pc))
+        tidal_radius_old = tidal_radius
+        while ((tidal_radius_old-tidal_radius)/tidal_radius_old>1e-2):
+            tidal_radius_old = tidal_radius
+            rt2 = tidal_radius*tidal_radius
+            rtsel = r2<=rt2
+            tidal_radius = gal_field.tidal_radius(4|units.pc, core.position.x, core.position.y, core.position.z, computer.particles[rtsel].total_mass())
+            # tidal_radius = (computer.particles[rtsel].total_mass()/(3*Mgal))**(1/3) * core.position.length()
+        
+        outside = computer.particles[r2 > tidal_radius**2]
+        # energies = 0.5*(outside.velocity-core.velocity).lengths()**2 + outside.potential_in_code
+        to_remove = outside.copy()#[energies > 0 | units.erg/units.kg]
+        cluster.remove_particle(to_remove)
+        if len(cluster) <100:
+            break
+        
+        print("number of bound particles", len(cluster))
+        print("number of unbound particles", len(to_remove))
+        computer.particles.remove_particles(to_remove)
+
+        data['time'].append(t_snap)
+        data['mass'].append(cluster.mass.sum())
+        data['mean_mass'].append(cluster.mass.mean())
+        data['galactocentric_radius'].append(cluster.center_of_mass().length())
+
+        cluster.move_to_center()
+        rhalf = cluster.LagrangianRadii(mf=[0.5])[0][0]
+        data['rhalf'].append(rhalf)
+        
+        # this could happen in parallel - would need threadfence at end
+        computer.particles.move_to_center()
+        potential_energy = (computer.particles.mass*computer.particles.potential_in_code).sum()
+        print("potential energy", potential_energy.in_(units.erg))
+        print("kinetic energy", computer.particles.kinetic_energy().in_(units.erg))
+        E = computer.particles.kinetic_energy() + potential_energy
+        data["E"].append(E)
+        data['kappa'].append(-E*rhalf/(constants.G*computer.particles.mass.sum()**2))
+        print("kappa", data['kappa'][-1])
+        inside = cluster.position.lengths() < rhalf
+        data['psi'].append((cluster.mass[inside]**(5/2)).mean()/cluster.mass[inside].mean()**(5/2))
+        computer.stop()
+        if galaxy_filename:
+            galaxy_force_field.stop()
     return data
 
 def model_cluster(filename, data):
@@ -149,7 +240,7 @@ def main(filename, cluster_file_type, galaxy_filename,outfile):
     data["RhJ"] = [] | units.pc
 
     if cluster_file_type == 'hdf5':
-        final_data = amuse_cluster(filename,galaxy_filename, data)
+        final_data = amuse_cluster_new(filename,galaxy_filename, data)
     elif cluster_file_type == 'txt':
         final_data = model_cluster(filename, data)
     else:
